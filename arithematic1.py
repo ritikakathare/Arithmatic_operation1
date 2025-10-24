4359629437d2f4206868e26747723185bf922040
ritikakathare <ritikakathare15@gmail.com>
2025-10-24 07:35:40 +0530

    Create arithematic1.py

---
+++ arithematic1.py
@@ -0,0 +1,14 @@
+# arithmetic.py
+# Program for Addition and Subtraction of two numbers
+
+# Input two numbers
+a = 10
+b = 5
+
+# Perform operations
+add = a + b
+sub = a - b
+
+# Display results
+print("Addition:", add)
+print("Subtraction:", sub)
